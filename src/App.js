0189c6db9e9849388e553d9f7bc5fe88b9bd8dcc
Alex Shestopal <ashestopal777@gmail.com>
2021-06-10 01:39:32 +0300

    map table

--- src/App.js
+++ src/App.js
@@ -2,22 +2,30 @@ import React from 'react';
 
 function App() {
 const users = [
-	{id:1, name: 'user1', surn: 'surn1', age: 30},
-	{id:2, name: 'user2', surn: 'surn2', age: 31},
-	{id:3, name: 'user3', surn: 'surn3', age: 32},
+	{id: 1, name: 'user1', surn: 'surn1', age: 30},
+	{id: 2, name: 'user2', surn: 'surn2', age: 31},
+	{id: 3, name: 'user3', surn: 'surn3', age: 32},
 ];
-	
-const res = users.map(function(item) {
-		return <li key={item.id}>
-			<span >{item.name + item.surn}</span>:
-			<span>{item.age}</span>
-		</li>;
+const rows = users.map(function(item) {
+		return <tr key={item.id}>
+			<td>{item.name}</td>
+			<td>{item.surn}</td>
+      <td>{item.age}</td>
+		</tr>;
 	});
 	
-	
-	return <ul>
-		{res}
-	</ul>;
+return <table>
+		<thead>
+			<tr>
+				<td>Ім'я</td>
+				<td>Прізвище</td>
+        <td>Вік</td>
+			</tr>
+		</thead>
+		<tbody>
+			{rows}
+		</tbody>
+	</table>;
 }
 
 export default App;
