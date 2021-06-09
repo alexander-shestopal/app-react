19288b47eb1e423d2186e12fc8418c53a095bf92
Alex Shestopal <ashestopal777@gmail.com>
2021-06-10 01:28:21 +0300

    map array key v.1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 function App() {
-	const arr = ['a', 'b', 'c', 'd', 'e'];
+const users = [
+	{name: 'user1', surn: 'surn1', age: 30},
+	{name: 'user2', surn: 'surn2', age: 31},
+	{name: 'user3', surn: 'surn3', age: 32},
+];
 	
-	const res = arr.map(function(item) {
-		return <li>{item}</li>;
+const res = users.map(function(item,val) {
+		return <li key={val}>
+			<span >{item.name + '   ' + item.surn}</span>:
+			<span>{item.age}</span>
+		</li>;
 	});
+	
 	
 	return <ul>
 		{res}
