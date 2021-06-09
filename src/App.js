05e29f49a1af52d8fd444cdae7bd26c5211c4f42
Alex Shestopal <ashestopal777@gmail.com>
2021-06-09 19:41:49 +0300

    app react test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let tmp, sum1 = 0; 
+function getDigitsSum(num){
+  while(num){
+    tmp  = num%10;
+    num = (num - tmp)/10;
+    sum1 += tmp;
+  }
+  return sum1;
+
+}
+	
+	return <div>
+		{getDigitsSum(12345)}
+	</div>
 }
 
 export default App;
