1e8a6e183d1380aac9aabda1d3bb55ae7865584f
Alex Shestopal <ashestopal777@gmail.com>
2021-06-10 02:05:38 +0300

    uuid and nanoid

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import uuid from 'react-uuid';
+import { nanoid } from 'nanoid';
 
 function App() {
 const users = [
 	{id: 1, name: 'user1', surn: 'surn1', age: 30},
 	{id: 2, name: 'user2', surn: 'surn2', age: 31},
 	{id: 3, name: 'user3', surn: 'surn3', age: 32},
+  	{id: 5, name: 'user5', surn: 'surn5', age: 35},
+    	{id: 8, name: 'user8', surn: 'surn8', age: 38},
 ];
 const rows = users.map(function(item) {
-		return <tr key={item.id}>
+		return <tr key={uuid()}>
 			<td>{item.name}</td>
 			<td>{item.surn}</td>
       <td>{item.age}</td>
