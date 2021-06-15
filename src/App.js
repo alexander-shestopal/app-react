35b4fdb620eaae497029d8ee0ecb75ccd6f8608d
Alex Shestopal <ashestopal777@gmail.com>
2021-06-15 15:20:43 +0300

    dt.17.6

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ function App() {
 	let [ban, setBan] = useState(false);
 	let [user, setUser] = useState('Petja');
 	let [lastName, setLastName] = useState('Petrov');
-	let age = 18;
+	let [age, setAge] = useState(18);
 	return <div>
 		<span>{user}</span>
 		<button onClick={()=>setUser('alex')}>changeUser</button>
 		<span>{lastName}</span>
 		<button onClick={()=>setLastName('Alexandrov')}>changeLastName</button>
+		<button onClick={() => setAge(age-1)}>-</button>
 		<span>{age}</span>
+		<button onClick={() => setAge(age+1)}>+</button>
 		<span>{ban?'Ban':'UnBan'}</span>
 		<button onClick={() => setBan(!ban)}>Ban/UnBan</button>
 	</div>;
