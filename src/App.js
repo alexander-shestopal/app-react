7c5937484fc63f925e0066f5a0f03b7d7326cafc
Alex Shestopal <ashestopal777@gmail.com>
2021-06-15 16:18:20 +0300

    dt18.2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React, { useState } from 'react';
 
 function App() {
-	let [ban, setBan] = useState(false);
-	let [user, setUser] = useState('Petja');
-	let [lastName, setLastName] = useState('Petrov');
-	let [age, setAge] = useState(18);
+	const [value, setValue] = useState('');
+	
 	return <div>
-		<span>{user}</span>
-		<button onClick={()=>setUser('alex')}>changeUser</button>
-		<span>{lastName}</span>
-		<button onClick={()=>setLastName('Alexandrov')}>changeLastName</button>
-		<button onClick={() => setAge(age-1)}>-</button>
-		<span>{age}</span>
-		<button onClick={() => setAge(age+1)}>+</button>
-		<span>{ban?'Ban':'UnBan'}</span>
-		<button onClick={() => setBan(!ban)}>Ban/UnBan</button>
+		<input value={value} onChange={event => setValue(event.target.value)} />
+		<p>text: {value.length}</p>
 	</div>;
 }
 
