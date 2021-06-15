b069fd090687c0aa81a32c5ab3f892ceb11b7476
Alex Shestopal <ashestopal777@gmail.com>
2021-06-15 16:43:56 +0300

    dt18.3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { useState } from 'react';
 
+function square(num) {
+	return num ** 2;
+}
+function age(year) {
+let date = new Date();
+let str = date.getFullYear();
+return str - year;
+}
+
 function App() {
-	const [value, setValue] = useState('');
+	const [value, setValue] = useState(0);
+	
+	function handleChange(event) {
+		setValue(event.target.value);
+	}
 	
 	return <div>
-		<input value={value} onChange={event => setValue(event.target.value)} />
-		<p>text: {value.length}</p>
+		<input value={value} onChange={handleChange} />
+		<p>{age(value)}</p>
 	</div>;
 }
 
