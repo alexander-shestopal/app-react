c994b5f1dd0c92c3b305632f07f8553ee95a5f83
Alex Shestopal <ashestopal777@gmail.com>
2021-06-15 17:03:09 +0300

    dt18.5

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
 import React, { useState } from 'react';
 
-function square(num) {
-	return num ** 2;
-}
-function age(year) {
-let date = new Date();
-let str = date.getFullYear();
-return str - year;
-}
-function gradus(farengeet) {
-	return (farengeet - 32)/1.8;
-}
-
 function App() {
-	const [value, setValue] = useState(0);
+	const [value1, setValue1] = useState(0);
+	const [value2, setValue2] = useState(0);
+	const [value3, setValue3] = useState(0);
+	const [value4, setValue4] = useState(0);
+	const [value5, setValue5] = useState(0);
 	
-	function handleChange(event) {
-		setValue(event.target.value);
+	function handleChange1(event) {
+		setValue1(+event.target.value);
+	}
+	
+	function handleChange2(event) {
+		setValue2(+event.target.value);
+	}
+		function handleChange3(event) {
+		setValue3(+event.target.value);
+	}
+	
+	function handleChange4(event) {
+		setValue4(+event.target.value);
+	}
+	function handleChange5(event) {
+		setValue5(+event.target.value);
 	}
 	
 	return <div>
-		<input value={value} onChange={handleChange} />
-		<p>{gradus(value)}</p>
-	</div>;
+		<input value={value1} onChange={handleChange1} />
+		<input value={value2} onChange={handleChange2} />
+		<input value={value3} onChange={handleChange3} />
+		<input value={value4} onChange={handleChange4} />
+		<input value={value5} onChange={handleChange5} />
+		<p>result: {(value1 + value2 + value3 + value4 + value5)/5}</p>
+	</div>
 }
 
 export default App;
