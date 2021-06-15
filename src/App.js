ada3035b2a6330500aeadcd5c793ef32d99b08f7
Alex Shestopal <ashestopal777@gmail.com>
2021-06-15 17:10:37 +0300

    dt19.1

--- src/App.js
+++ src/App.js
@@ -3,36 +3,16 @@ import React, { useState } from 'react';
 function App() {
 	const [value1, setValue1] = useState(0);
 	const [value2, setValue2] = useState(0);
-	const [value3, setValue3] = useState(0);
-	const [value4, setValue4] = useState(0);
-	const [value5, setValue5] = useState(0);
-	
-	function handleChange1(event) {
-		setValue1(+event.target.value);
-	}
-	
-	function handleChange2(event) {
-		setValue2(+event.target.value);
-	}
-		function handleChange3(event) {
-		setValue3(+event.target.value);
-	}
-	
-	function handleChange4(event) {
-		setValue4(+event.target.value);
-	}
-	function handleChange5(event) {
-		setValue5(+event.target.value);
-	}
+	const [result, setResult] = useState(0);
 	
 	return <div>
-		<input value={value1} onChange={handleChange1} />
-		<input value={value2} onChange={handleChange2} />
-		<input value={value3} onChange={handleChange3} />
-		<input value={value4} onChange={handleChange4} />
-		<input value={value5} onChange={handleChange5} />
-		<p>result: {(value1 + value2 + value3 + value4 + value5)/5}</p>
-	</div>
+		<input value={value1} onChange={event => setValue1(event.target.value)} />
+		<input value={value2} onChange={event => setValue2(event.target.value)} />
+		
+		<button onClick={() => setResult(Number(value1) + Number(value2))}>btn+</button>
+		<p>result: {result}</p>
+		<button onClick={() => setResult(Number(value1)*Number(value2))}>btn*</button>
+	</div>;
 }
 
 export default App;
