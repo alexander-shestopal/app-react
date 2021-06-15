5df24ee6e336cdaa7e911ec326d3272c95f3e695
Alex Shestopal <ashestopal777@gmail.com>
2021-06-15 22:09:41 +0300

    dt19.2

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ function App() {
 	const [value1, setValue1] = useState(0);
 	const [value2, setValue2] = useState(0);
 	const [result, setResult] = useState(0);
-	
+	function diffDates(day_one, day_two) {
+		var d1 = new Date(day_one) //firstDate
+		var d2 = new Date(day_two) //SecondDate
+		var diff = Math.abs(d1-d2)/(1000*3600*24); //in milliseconds
+    	return diff;
+	};
 	return <div>
-		<input value={value1} onChange={event => setValue1(event.target.value)} />
-		<input value={value2} onChange={event => setValue2(event.target.value)} />
+		<input type="date" value={value1} onChange={event => setValue1(event.target.value)} />
+		<input type="date" value={value2} onChange={event => setValue2(event.target.value)} />
+
+		<button onClick={() => setResult( diffDates(value2,value1))}>date</button>
 		
-		<button onClick={() => setResult(Number(value1) + Number(value2))}>btn+</button>
 		<p>result: {result}</p>
-		<button onClick={() => setResult(Number(value1)*Number(value2))}>btn*</button>
 	</div>;
 }
 
