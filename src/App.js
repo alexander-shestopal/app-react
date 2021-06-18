26737a68f42544cbb34ad079c4885d110f9bfb6a
Alex Shestopal <ashestopal777@gmail.com>
2021-06-18 11:49:07 +0300

    dt26.1

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
 import React, { useState } from 'react';
 
-function currentDates() {
-	const today = new Date();
-	let month = today.getMonth()+1;
-	let pref = "-0";
-	if(month>9){
-		let pref = "-";
-	}
-	const date = today.getFullYear() + pref + month +"-" + today.getDate();
-	return date; //firstDate
-};
+
 function App() {
-	const [value1, setValue1] = useState(currentDates());
-	const [value2, setValue2] = useState(currentDates());
-	const [result, setResult] = useState(0);
-	function diffDates(day_one, day_two) {
-		var d1 = new Date(day_one) //firstDate
-		var d2 = new Date(day_two) //SecondDate
-		var diff = Math.abs(d1-d2)/(1000*3600*24) //in milliseconds/days
-    	return diff;
-	};
-
-	return <div>
-		<input type="date" value={value1} onChange={event => setValue1(event.target.value)} />
-		<input type="date" value={value2} onChange={event => setValue2(event.target.value)} />
-
-		<button onClick={() => setResult( diffDates(value2,value1))}>date</button>
+	const [notes, setNotes] = useState([1, 2, 3, 4, 5]);
+	const [value, setValue] = useState('');
+	
+	const result = notes.map((note, index) => {
+		return <li key={index}>{note}</li>;
+	});
+	
+	function addItem() {
+		setNotes([...notes, value]);
+	}
+	
+	function changeInput(event) {
+		setValue(event.target.value);
+	}
+	
+	return <ul>
+		{result}
 		
-		<p>result: {result}</p>
-	</div>;
+		<input value={value} onChange={changeInput} />
+		<button onClick={addItem}>add</button>
+	</ul>;
 }
 
 export default App;
